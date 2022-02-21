286f0680c9800968ac0e8d23c185b81893e27c5f
EmiirBulut <emirr.bulut@outlook.com>
2022-02-21 14:30:27 +0300

    Entitiy Framework Katmanı eklendi ve tum entityler icin interfaceler olusturuldu

---
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICategoryService
+    {
+        void CategoryAdd(Category category); // Not : Burada EntityLayer.Concrate kullanabilmek icin Dependencies --> Project kısmına referansın eklenmiş olması gerekiyor
+        void CategoryDelete(Category category);
+        void CategoryUpdate(Category category);
+        List<Category> GetCategories();
+        Category GetByID(int id);
+    }
+}
